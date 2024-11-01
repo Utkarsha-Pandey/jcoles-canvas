3bb61c5a3f749e343e04db5b2831553a5a32a7a9
LakshyaPuripanda <lakshya.20223523@mnnit.ac.in>
2024-11-01 23:41:19 +0530

    opacity tool

--- src/features/editor/components/opacity-sidebar.tsx
+++ src/features/editor/components/opacity-sidebar.tsx
@@ -2,37 +2,34 @@ import {
     ActiveTool,
     Editor,
     STROKE_COLOR,
-    STROKE_DASH_ARRAY,
-    STROKE_WIDTH,
 } from "@/features/types";
 import { cn } from "@/lib/utils";
 import { ToolSidebarHeader } from "./tool-sidebar-header";
 import { ToolSidebarClose } from "./tool-sidebar-close";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Label } from "@/components/ui/label";
-import { Slider } from "@/components/ui/slider";
-import { Button } from "@/components/ui/button";
 
-interface StrokeWidthSidebarProps {
+import { Slider } from "@/components/ui/slider";
+
+
+interface OpacitySidebarProps {
     editor: Editor | undefined;
     activeTool: ActiveTool;
     onChangeActiveTool: (tool: ActiveTool) => void;
 }
 
-export const StrokeWidthSidebar = ({
+export const OpacitySidebar = ({
     editor,
     activeTool,
     onChangeActiveTool,
-}: StrokeWidthSidebarProps) => {
-    const widhtValue = editor?.getActiveStrokeWidth() || STROKE_WIDTH;
-    const typeValue = editor?.getActiveStrokeDashArray() || STROKE_DASH_ARRAY;
+}: OpacitySidebarProps) => {
+    const value = editor?.getActiveOpacity() || 1;
 
     const onClose = () => {
         onChangeActiveTool("select");
     };
 
     const onChangeStrokeWidth = (value: number) => {
-        editor?.changeStrokeWidth(value);
+        editor?.changeOpacity(value);
     };
 
     const onChangeStrokeType = (value: number[]) => {
@@ -43,50 +40,27 @@ export const StrokeWidthSidebar = ({
         <aside
             className={cn(
                 "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
-                activeTool === "stroke-width" ? "visible" : "hidden"
+                activeTool === "opacity" ? "visible" : "hidden"
             )}
         >
             <ToolSidebarHeader
-                title="Stroke options"
-                description="Modify stroke of your object"
+                title="Opacity"
+                description="Change the opacity of the selected object"
             />
             <ScrollArea>
                 <div className="p-4 space-y-4 border-b">
-                    <Label className="text-sm">Stroke Width</Label>
+
                     <Slider
-                        value={[widhtValue]}
+                        value={[value]}
                         onValueChange={(values) =>
-                            onChangeStrokeWidth(values[0])
+                            onChange(values[0])
                         }
+                        max={1}
+                        min={0}
+                        step={0.01}
                     />
                 </div>
 
-                <div className="p-4 space-y-4 border-b">
-                    <Label className="text-sm">Stroke Type</Label>
-                    <Button
-                        onClick={() => onChangeStrokeType([])}
-                        variant="secondary"
-                        size="lg"
-                        className={cn("w-full h-16 justify-start text-left" , JSON.stringify(typeValue) === `[5,5]` && "border-2 border-blue-500") }
-                        style={{
-                            padding: "8px 16px",
-                        }}
-                    >
-                        <div className="w-full border-black rounded-full border-4" />
-                    </Button>
-
-                    <Button
-                        variant="secondary"
-                        size="lg"
-                        className="w-full h-16 justify-start text-left"
-                        style={{
-                            padding: "8px 16px",
-                        }}
-                        onClick={() => onChangeStrokeType([5, 5])}
-                    >
-                        <div className="w-full border-black rounded-full border-4 border-dashed" />
-                    </Button>
-                </div>
             </ScrollArea>
             <ToolSidebarClose onClick={onClose} />
         </aside>
