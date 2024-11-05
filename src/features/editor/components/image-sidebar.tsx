4aa3526bd7c284650ed2f5f620ee8b6c65e26182
bhujo <utkarshapandey2004@gmail.com>
2024-11-05 12:42:04 +0530

    upload image in the canvas

--- src/features/editor/components/image-sidebar.tsx
+++ src/features/editor/components/image-sidebar.tsx
@@ -1,4 +1,4 @@
-import { ActiveTool, Editor } from "@/features/types";
+ import { ActiveTool, Editor } from "@/features/types";
 import { cn } from "@/lib/utils";
 import { ToolSidebarHeader } from "./tool-sidebar-header";
 import { ToolSidebarClose } from "./tool-sidebar-close";
@@ -7,6 +7,7 @@ import { useGetImages } from "@/features/images/api/use-get-images";
 import { AlertTriangle, Loader } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { UploadButton } from "@/lib/uploadthing";
 
 interface ImageSidebarProps {
     editor: Editor | undefined;
@@ -36,6 +37,21 @@ export const ImageSidebar = ({
                 title="Images"
                 description="Add images to your canvas"
             />
+            <div className="p-4 border-b">
+               <UploadButton
+               appearance={{
+                button: "w-full text-sm font-medium",
+                allowedContent:"hidden"
+               }}
+               content={{
+                button: "upload Image"
+               }}
+               endpoint ="imageUploader"
+               onClientUploadComplete={(res)=>{
+                editor?.addImage(res[0].url);
+               }}
+               />
+            </div>
             {isLoading && (
                 <div className="flex items-center justify-center flex-1">
                     <Loader className="size-4 text-muted-foreground animate-spin" />
