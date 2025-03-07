84279b9a6bd79a587af2fcfb62f0b2c9a2470e28
Utkarsha Pandey <utkarshapandey2004@gmail.com>
2025-03-07 20:44:10 +0530

    database

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,15 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import {DrizzleAdapter} from "@auth/drizzle-adapter";
+import {db} from "./db/drizzle";
+import { accounts, users } from "./db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    
+  }),
   providers: [GitHub, Google]
-})
+});
